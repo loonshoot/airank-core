c8cb7948f7e3ca2800b105972206db49682dcf3f
Grayson <grayson.campbell@gmail.com>
2025-06-21 20:54:11 +1000

    Cleanup on unnecessary services.

--- stream/src/models/inputs.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const inputsSchema = new mongoose.Schema({
-  name: String,
-  type: String,
-  validation: String
-});
-
-module.exports = { inputsSchema }; 
